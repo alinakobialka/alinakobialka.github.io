6eeade900b5525149d7fb4ea830280d6d37676f1
momo <asdf@asdf.com>
2021-10-09 14:29:51 -0700

    format

--- index.js
+++ index.js
@@ -3,24 +3,24 @@
 // balloons pop up if number is right
 
 (() => {
-    var biggest = 100
+  var biggest = 100;
 
-    function getAnswer() {
-        return Math.ceil(Math.random() * biggest)
+  function getAnswer() {
+    return Math.ceil(Math.random() * biggest);
+  }
+
+  var answer = getAnswer();
+
+  function guess() {
+    var value = parseInt(document.getElementById("input").value);
+    if (value > answer) {
+      alert("too high");
+    } else if (value < answer) {
+      alert("too low");
+    } else if (value === answer) {
+      alert("balloon");
     }
-
-    var answer = getAnswer()
-
-    function guess() {
-        var value = parseInt(document.getElementById("input").value)
-        if (value > answer) {
-            alert("too high")
-        } else if (value < answer) {
-            alert("too low")
-        } else if (value === answer) {
-            alert("balloon")
-        }
-        return false
-    }
-    document.getElementById("form").onsubmit = guess
-})()
+    return false;
+  }
+  document.getElementById("form").onsubmit = guess;
+})();
