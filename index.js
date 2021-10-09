0149acb8aa16db600266534a8bc81d29c432114f
momo <asdf@asdf.com>
2021-10-09 13:22:12 -0700

    progress

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-alert(0)
+// guess Number 1-100
+// clue pops up
+// balloons pop up if number is right
+
+function getAnswer() {
+    return 77
+}
+
+var answer = getAnswer()
+
+function guess() {
+    var value = document.getElementById("input").value
+    if (value > answer) {
+        alert("too high")
+    } else if (value < answer) {
+        alert("too low")
+    } else if (value === answer) {
+        alert("balloon")
+    }
+}
+document.getElementById("button").onclick = guess
