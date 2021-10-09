0a6e2cdd3d17c267c96d2cd0aa5a0cb7c6e1baf6
momo <asdf@asdf.com>
2021-10-09 14:26:40 -0700

    final

--- index.js
+++ index.js
@@ -2,22 +2,25 @@
 // clue pops up
 // balloons pop up if number is right
 
-function getAnswer() {
-    // TODO
-    return 77
-}
+(() => {
+    var biggest = 100
 
-var answer = getAnswer()
-
-function guess() {
-    var value = parseInt(document.getElementById("input").value)
-    if (value > answer) {
-        alert("too high")
-    } else if (value < answer) {
-        alert("too low")
-    } else if (value === answer) {
-        alert("balloon")
+    function getAnswer() {
+        return Math.ceil(Math.random() * biggest)
     }
-    return false
-}
-document.getElementById("form").onsubmit = guess
+
+    var answer = getAnswer()
+
+    function guess() {
+        var value = parseInt(document.getElementById("input").value)
+        if (value > answer) {
+            alert("too high")
+        } else if (value < answer) {
+            alert("too low")
+        } else if (value === answer) {
+            alert("balloon")
+        }
+        return false
+    }
+    document.getElementById("form").onsubmit = guess
+})()
